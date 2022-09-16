b727257a5e39470b8ab8843ffdb6a9bedd8c6f64
batch <lucashernandez904@gmail.com>
2022-09-16 15:33:42 -0300

    build update

--- build/src/App.js
+++ build/src/App.js
@@ -3,13 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
-const PORT = 3000;
-app.get('/pong', (req, res) => {
-    res.send('pong');
-});
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+require('dotenv').config();
+const server_1 = __importDefault(require("./models/server"));
+const server = new server_1.default();
+server.listen();
