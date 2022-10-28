033215c309b3ad75e92043689cc6630da3db3ab0
batch <lucashernandez904@gmail.com>
2022-10-28 15:20:17 -0300

    add: vercel json

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express'
 import { check } from 'express-validator'
-import { login } from '../controllers/auth'
+
+import { login, googleSignIn } from '../controllers/auth'
+import { validateFields } from '../../middlewares/validate-fields';
 
 export const authRoutes = Router()
 
 authRoutes.post('/login', [
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'El password es obligatorio').not().isEmpty(),
+    validateFields
 ], login)
+
+authRoutes.post('/google', [
+    check('id_token', 'El id_token de Google es obligatorio').not().isEmpty(),
+    validateFields
+], googleSignIn)
