d0f3a0d0043c4d09146846e68f5631547333b947
batch <lucashernadez904@gmail.com>
2023-11-16 11:41:11 -0300

    feat: initial vercel configuration

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-import Server from './models/server'
-import TelegramBotMLHOY from './models/telegramBot/telegramBot';
-
-const server = new Server()
-export const telegramBotInstance = new TelegramBotMLHOY()
-
-server.listen()
-telegramBotInstance.initialize()
