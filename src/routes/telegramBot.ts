42d673b6210bf08f516b1b9ae75d42bb3ac52985
batch <lucashernadez904@gmail.com>
2023-11-16 11:49:21 -0300

    fix: routes telegram bot was commented

--- src/routes/telegramBot.ts
+++ src/routes/telegramBot.ts
@@ -1,12 +1,13 @@
-import { activateAutomaticMessages } from "../controllers/telegramBot"
-import { Router } from "express"
-import { validateFields } from "../../middlewares/validate-fields"
-import { validateJWT } from "../../middlewares/validate-jwt"
+// // import { activateAutomaticMessages } from "../controllers/telegramBot"
+// import { Router } from "express";
+// import { validateFields } from "../../middlewares/validate-fields";
+// import { validateJWT } from "../../middlewares/validate-jwt";
 
-export const botTelegramRoutes = Router()
+// export const botTelegramRoutes = Router();
 
-// Activate automatic messages
-botTelegramRoutes.post('/activateAutomaticMessages',[
-    validateJWT,
-    validateFields,
-], activateAutomaticMessages)
+// // Activate automatic messages
+// botTelegramRoutes.post(
+//   "/activateAutomaticMessages",
+//   [validateJWT, validateFields],
+//   activateAutomaticMessages
+// );
